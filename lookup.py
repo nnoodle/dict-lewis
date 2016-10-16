ad5bb59b2481ac88c63d01aa46f602298dc915b1
Ian Douglas Scott <ian@iandouglasscott.com>
2016-10-15 18:46:28 -0700

    Remove extra whitespace at end

--- lookup.py
+++ lookup.py
@@ -4,5 +4,5 @@ import sys
 with open("lewis.json") as dicfile:
     dictionary = json.load(dicfile)
 
-for definition in dictionary[sys.argv[1].lower()]:
-    print(definition)
+definitions = dictionary[sys.argv[1].lower()]
+print(*definitions, end='')
